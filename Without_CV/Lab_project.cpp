0fe74ad4460ecf3beac89069f8bf9e59c26cbdf4
Abdul6498 <73305650+Abdul6498@users.noreply.github.com>
2021-07-09 11:07:03 +0200

    Optimized code with coarse pixel position

--- Without_CV/Lab_project.cpp
+++ Without_CV/Lab_project.cpp
@@ -1,129 +1,87 @@
 
-#include <vec_operation.h>
+#include "vec_operation.h"
 
-#define LOG(x) std::cout << x << std::endl;
-#define LOG_w(x) std::cout << x << " , ";
-#define newline() std::cout << std::endl;
-#define GetValue(x) std::cin >> x ;
-#define line(x) std::cout << x;
-
-unsigned volatile kernel_size = 3;     //Kernel Size for averging filter
-size_t window = 10;
 std::pair<float, float> src(0, 50), dst(0, 1);
 
-template<typename tt>
-tt SAD_disparity(tt& imageL, tt& imageR, tt& disp_img, int countX, int countY)
-{
-	std::size_t count = window * window;
-	std::vector<float> L(count);
-	std::vector<float> R(count);
-	std::vector<float> diff(count);
-	float sum;
-	float disparity = 0;
-	for (int i = 48; i < (int)countX; i++) {
-		for (int j = 0; j < (int)countY; j++) {
-			float min = 10000.01;
-			vec::Rect<std::vector<float>>(imageL, i, j, window, countX, countY, L);
-			for (int t = i - 48; t < i; t++) {
-				vec::Rect<std::vector<float>>(imageR, t, j, window, countX, countY, R);
-				vec::vec_subtract<std::vector<float>>(L, R, diff, window);
-				sum = vec::vec_sum<std::vector<float>>(diff, window);
-				if (min > sum) {
-					min = sum;
-					disparity = i - t;
-					//  point_x = x;
-					//  point_t = t;
-				}
-			}
-
-			disparity = vec::map_value(src, dst, disparity);
-			disp_img[vec::getIndexGlobal(countX, i, j)] = disparity;
-		}
-	}
-	return disp_img;
-}
-
-template<typename tt>
-tt NCC_disparity(tt& imageL, tt& imageR, tt& disp_img, int countX, int countY)
-{
-	std::size_t count = window * window;
-	std::vector<float> L(count);
-	std::vector<float> R(count);
-	std::vector<float> diff(count);
-	std::vector<float> R_sq(count);
-	std::vector<float> L_sq(count);
-	std::vector<float> prod(count);
-	float sum;
-	float disparity = 0;
-	for (int i = 48; i < (int)countX; i++) 
-	{
-		for (int j = 0; j < (int)countY; j++) 
-		{
-			float max = 0.000001;
-			vec::Rect<std::vector<float>>(imageL, i, j, window, countX, countY, L);
-			for (int t = i - 48; t < i; t++) 
-			{
-				vec::Rect<std::vector<float>>(imageR, t, j, window, countX, countY, R);
-				vec::vec_multiply(L, R, prod, window);
-				auto summ = vec::vec_sum<std::vector<float>>(prod, window);
-				vec::vec_multiply(L, L, L_sq, window);
-				vec::vec_multiply(R, R, R_sq, window);
-				auto denom = std::sqrt(vec::vec_sum<std::vector<float>>(L_sq,window) * vec::vec_sum<std::vector<float>>(R_sq, window));
-				auto norm = summ / denom;
-				if (norm > max) {
-					max = norm;
-					disparity = i - t;
-					//  point_x = x;
-					//  point_t = t;
-				}
-			}
-			// sum = 0;
-		   //  for (int p_x = point_x; p_x < 3; p_x++)
-		   //  {
-			   //  sum = int(imageL.at<uchar>(cv::Point(p_x, y))) + sum;
-			   //  cog = sum / 3;
-		   //  }
-			disparity = vec::map_value(src, dst, disparity);
-			disp_img[vec::getIndexGlobal(countX, i, j)] = disparity;
-		}
-	}
-	return disp_img;
-}
-
+enum disp_algo {
+	SAD_Disprity,
+	NCC_Disparity
+};
 
 int main(int argc, char** argv) {
 
-	std::size_t wgSizeX = 16; // Number of work items per work group in X direction
-	std::size_t wgSizeY = 16;
-	//std::size_t countX = wgSizeX * 40; // Overall number of work items in X direction = Number of elements in X direction
-	//std::size_t countY = wgSizeY * 30;
-	std::size_t countX = 450; // Overall number of work items in X direction = Number of elements in X direction
-	std::size_t countY = 375;
-	std::size_t count = countX * countY; // Overall number of elements
-	std::vector<float> imageL(count);
-	std::vector<float> imageR(count);
-	std::vector<float> imageD(count);
-	
+	int idx = -1;	//golobal index for total number of files is directory
+	int idx_L = -1;	//Get user index for left image
+	int idx_R = -1;	//Get user index for Right image
+
+	stereo stereo;	// Class for stereo functions
+	stereo.window = 10;	//window size
+	stereo.dmax = 50;	//Max diparity
+	std::vector < std::string > filename;	//Variable for file name
+
+	std::string path = "../../../../Opencl-ex1/images/";	//Path for image directory ends with/
+	idx = stereo.load_files(path, filename);	//Load files
+
+
+	line("Select Left image index: ");
+	GetValue(idx_L);
+	line("Select Right image index: ");
+	GetValue(idx_R);
+
+	if (idx_L > idx || idx_R > idx)
 	{
-		std::vector<float> inputData;
-		std::size_t inputWidth, inputHeight;
-		Core::readImagePGM("D:/INFOTECH/2nd Semester/High Performance Programming with graphic cards/Exercise/Opencl-ex1/Opencl-ex1/im2.pgm", inputData, inputWidth, inputHeight);
-		for (size_t j = 0; j < countY; j++) {
-			for (size_t i = 0; i < countX; i++) {
-				imageL[i + countX * j] = inputData[(i % inputWidth) + inputWidth * (j % inputHeight)];
+		std::cerr << "Error! Input indexes are out of Range \n";
+		return -1;
+	}
+	else if (idx_L == idx_R)
+	{
+		std::cerr << "Warning! You are comparing the same image \n";
+	}
+
+
+	
+	std::vector<float> inputData;	//Variable for image data
+	std::size_t inputWidth, inputHeight;	//Variables for width and height of image
+	Core::readImagePGM(filename[idx_L], inputData, inputWidth, inputHeight);	//Read image dat from the specific path
+	std::size_t countX = inputWidth;	
+	std::size_t countY = inputHeight;
+
+	std::vector<float> imageL(inputData.size());	//Left image
+	std::vector<float> imageR(inputData.size());	//Right image
+	std::vector<float> imageD(inputData.size());	//Disparity map
+
+	{
+		//Read Left image Data
+		for (size_t j = 0; j < inputHeight; j++) {
+			for (size_t i = 0; i < inputWidth; i++) {
+				imageL[i + inputWidth * j] = inputData[(i % inputWidth) + inputWidth * (j % inputHeight)];
 			}
 		}
-		Core::readImagePGM("D:/INFOTECH/2nd Semester/High Performance Programming with graphic cards/Exercise/Opencl-ex1/Opencl-ex1/im6.pgm", inputData, inputWidth, inputHeight);
-		for (size_t j = 0; j < countY; j++) {
-			for (size_t i = 0; i < countX; i++) {
-				imageR[i + countX * j] = inputData[(i % inputWidth) + inputWidth * (j % inputHeight)];
+		Core::readImagePGM(filename[idx_R], inputData, inputWidth, inputHeight);
+
+		//Read Right image Data
+		for (size_t j = 0; j < inputHeight; j++) {
+			for (size_t i = 0; i < inputWidth; i++) {
+				imageR[i + inputWidth * j] = inputData[(i % inputWidth) + inputWidth * (j % inputHeight)];
 			}
 		}
 	}
-		SAD_disparity<std::vector<float>>(imageL, imageR, imageD, countX, countY);
+		auto sad_start = std::chrono::high_resolution_clock::now();
+
+		stereo.SAD_disparity<std::vector<float>>(imageL, imageR, imageD, countX, countY);	//SAD Disparity Calculation
+
+		auto sad_end = std::chrono::high_resolution_clock::now();
+		auto sad_time = std::chrono::duration_cast<std::chrono::seconds>(sad_end - sad_start).count();
+
 		Core::writeImagePGM("SAD_out.pgm", imageD, countX, countY);
-		NCC_disparity<std::vector<float>>(imageL, imageR, imageD, countX, countY);
-		Core::writeImagePGM("NCC_out.pgm", imageD, countX, countY);
+
+		auto ncc_start = std::chrono::high_resolution_clock::now();
+		stereo.NCC_disparity<std::vector<float>>(imageL, imageR, imageD, countX, countY);	//NCC Disparity Calculation
+		auto ncc_end = std::chrono::high_resolution_clock::now();
+		auto ncc_time = std::chrono::duration_cast<std::chrono::seconds>(ncc_end - ncc_start).count();
+
+		Core::writeImagePGM("NCC_out.pgm", imageD, countX, countY);	
+
 	LOG("Completed");
 	return 0;
 }
--- Without_CV/vec_operation.cpp
+++ Without_CV/vec_operation.cpp
@@ -1,11 +1,13 @@
 #include "vec_operation.h"
-
 namespace vec {
+
+	void show_options() {
+		//Write Options
+	}
 
 	int getIndexGlobal(std::size_t countX, int i, int j) {
 		return j * countX + i;
 	}
-
 
 	float getValueGlobal(const std::vector<float>& img, std::size_t countX, std::size_t countY, int i, int j) {
 		if (i < 0 || (size_t)i >= countX || j < 0 || (size_t)j >= countY)
@@ -24,4 +26,35 @@ namespace vec {
 		std::cout << "Speed Up with memory copy : " << speedup_w_m << std::endl;
 	}
 
+}
+int stereo::find_idx(std::string& str, std::string& s)
+{
+
+	bool flag = false;
+	for (int i = 0; i < str.length(); i++) {
+		if (str.substr(i, s.length()) == s) {
+			pos = i;
+			flag = true;
+		}
+	}
+	if (flag == false) {
+		LOG("NONE");
+	}
+	return pos;
+}
+
+int stereo::load_files(std::string path, std::vector < std::string >& filename) {
+	int i = 0;
+	for (auto& entry : std::filesystem::directory_iterator(path))
+	{
+		std::string path_string{ entry.path().u8string() };
+		filename.push_back(path_string);
+		std::string str1 = "/";
+		pos = find_idx(filename[i], str1) + 1;
+		std::string str2 = filename[i].substr(pos, filename[i].length());
+		line("Index# "); line(i); line(" ");  LOG(str2);
+		i++;
+
+	}
+	return i-1;
 }
